5ffde09ab3576589a88d3f14dbd9ed03d9f93c8f
Sylux <9683460+Sylux6@users.noreply.github.com>
2019-12-05 16:46:43 +0100

    pre-release

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
 # AzurApi-Kotlin
 A Kotlin wrapper for Azur Lane JSON
 
+## Use
+<details><summary>Kotlin (build.gradle.kts)</summary>
+
+```kotlin
+repositories {
+    maven(url = "https://jitpack.io")
+}
+```
+
+```kotlin
+dependencies {
+    implementation("com.github.AzurAPI:AzurApi-Kotlin:Tag")
+}
+```
+</details>
+
+<details><summary>Maven</summary>
+
+```xml
+<repositories>
+    <repository>
+        <id>jitpack.io</id>
+        <url>https://jitpack.io</url>
+    </repository>
+</repositories>
+```
+
+```xml
+<dependency>
+    <groupId>com.github.AzurAPI</groupId>
+    <artifactId>AzurApi-Kotlin</artifactId>
+    <version>Tag</version>
+</dependency>
+```
+</details>
+
 ## Support server
 ![Discord Banner 2](https://discordapp.com/api/guilds/648206344729526272/widget.png?style=banner2)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,18 @@ val fuelVersion = "2.2.1"
 val kotlintestVersion = "3.4.2"
 
 plugins {
+    java
+    maven
     kotlin("jvm") version "1.3.61"
 }
 
 group = "com.github.azurapi.azurapikotlin"
-version = "v1.0.0"
+version = "0.1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 repositories {
     mavenCentral()
@@ -23,15 +30,14 @@ dependencies {
     testImplementation("io.kotlintest:kotlintest-runner-junit5:$kotlintestVersion")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
     }
-    compileTestKotlin {
+    compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
 }
